3179635fc8df12acecbaa849ffaf07e2de15cff5
RicardFabien <49694912+RicardFabien@users.noreply.github.com>
2021-12-08 12:30:17 +0100

    Modified main

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-import html
-import pandas as pa
-import sklearn
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.pipeline import make_pipeline
-from sklearn.svm import SVC
+import joblib
+import flask
 
-comments = pa.read_csv("labels.csv")
-comments = comments.sample(1000)
+model = joblib.load("model.pkl")
 
-tweets = comments["tweet"].transform(lambda line: html.unescape(line))
-
-comments.drop('tweet', axis=1, inplace=True)
-
-X = tweets
-y = comments[ ["offensive_language","hate_speech"]]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.80, random_state=413)
-
-clf = make_pipeline(
-    TfidfVectorizer(stop_words='english'),
-    OneVsRestClassifier(SVC(kernel='linear', probability=True))
-)
-
-clf.fit_transform(X_train,y_train)
+classification = {0 : "Contenue haineux",1: "Language offensant",2 : "Contenue benin"}
